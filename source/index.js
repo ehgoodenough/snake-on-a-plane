633981815a9f7d673c6fd1be5ec0c15fa9debfa5
Andrew McPherson <mcphandr@amazon.com>
2016-03-13 14:05:58 -0700

    Added a game state mock to bootstrap rendering.

--- source/index.js
+++ source/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World!")
+var React = require("react")
+var ReactDOM = require("react-dom")
+var Afloop = require("afloop")
+var Keyb = require("keyb")
+
+var state = {
+    game: {
+        snake: {
+            age: 3,
+            direction: "EAST",
+            pods: [
+                {
+                    age: 0,
+                    position: {
+                        x: 2,
+                        y: 2,
+                    }
+                },
+                {
+                    age: 1,
+                    position: {
+                        x: 1,
+                        y: 2,
+                    }
+                },
+                {
+                    age: 2,
+                    position: {
+                        x: 1,
+                        y: 3,
+                    }
+                },
+            ]
+        }
+    },
+    frame: {
+        width: 12,
+        height: 12,
+    }
+}
